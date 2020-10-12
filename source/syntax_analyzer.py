a3392fee5b34fb8c3eea0feb5d1f4785dd43fa38
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-12 17:05:53 +1100

    Update syntax_analyzer.py

--- source/syntax_analyzer.py
+++ source/syntax_analyzer.py
@@ -1,10 +1,23 @@
 # This may later be written in a different language.
 from lexfile import *
 
+analyzed = []
 class DeclaringErrror(Exception):
     pass
 def currentline():
     return line_list[line_list.index(line)]
+
+def modify_token(tok=token, old, new):
+    analyzed = analyzed.append(tok.replace(old, new))
+
+def mod_replace(t=token, oldvalue, newvalue, extra=False):
+    if t == token: 
+        t = token == oldvalue
+    
+    if t:
+        if extra != false:
+           extra()
+        modify_token(oldvalue, newvalue)
 
 for line in line_list:
     for token in lex_token:
@@ -15,6 +28,9 @@ for line in line_list:
                 before assignment. Define the variable with ':=', '=' is reserved for reassigning a value.")
             if token == "fn" and token_front() == kw or token == ":=" and token_front() == kw:
                 raise DeclaringError(f"Attempt to declare function or variable with name of a reserved keyword, '{kw}'\
-                at '{token}{token_front()}' in line {currentline()}.\
+                 at '{token}{token_front()}' in line {currentline()}.\
                  Consider renaming the variable or function to an unreserved keyword.")
-            
+
+            # Analyze
+            mod_replace(token == ":=" and token_behind() not in token_splitters, ":=", "=")
+            mod_replace(token == "fn" and token_front() not in token_splitters, "fn", "def", lambda
