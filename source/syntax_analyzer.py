1013d9307fb3d9ea4d44315c2d077781ccc96c11
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-20 20:17:24 +1100

    Moved all functions to ‘functions’ subdir.

--- source/syntax_analyzer.py
+++ source/syntax_analyzer.py
@@ -1,38 +1,8 @@
 # This may later be written in a different language.
 from lexfile import *
-
+from functions import *
 analyzed = []
 numlist = [1,2,3,4,5,6,7,8,9,0]
-
-def currentline():
-    return line_list[line_list.index(line)]
-
-def modify_token(tok=token, old, new):
-    analyzed = analyzed.append(tok.replace(old, new))
-
-def mod_replace(t=token, oldvalue, newvalue, extra=False):
-    if t == token: 
-        t = token == oldvalue
-    
-    if t:
-        if extra != false:
-           extra()
-        modify_token(oldvalue, newvalue)
-
-def linemod(old, new):
-    analyzed = analyzed.append(line.replace(old, new))
-
-def type_analyze():
-    if ":=" in line:
-        for numvalue in numlist:
-            tkswitch = {
-                        token_front().startswith('"') or token_behind.startswith("'"):\
-                        lambda: analyzed.append(line.replace(line, f'char[] {line}').replace(":=", "="))
-                        lambda: token_front().startswith(num): analyzed.append(line.replace(line, f'int {line}').replace(":=", "="))
-                       }
-                    for k,v in tkswitch.items():
-                        if k:
-                            v()
 
 
 # all iterations we'd ever need. 
