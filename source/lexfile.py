65e45196417eb449d3ee6b8c9e5c43da2f5b71df
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-20 20:15:58 +1100

    Update lexfile.py

--- source/lexfile.py
+++ source/lexfile.py
@@ -1,4 +1,5 @@
 # This may later change to a different language.
+from functions import *
 toparse = """
 []()
 """.replace("\n", "")
@@ -33,46 +34,7 @@ for token_splitter in token_splitters:
     tok = toparse.replace(token_splitter, "TOKEN_SPLIT")
     lex_token = tok.split("TOKEN_SPLIT")
 
-
-# Functions
-def line_above(count=1):
-    return line_list[line_list.index(line) + count]
-
-def line_below(count=1):
-    return line_list[line_list.index(line) - count]
-
-def space_front(count=1):
-    return space_tokenlist[space_tokenlist.index(space_token) + count]
-
-def space_behind(count=1):
-    return space_tokenlist[space_tokenlist.index(space_token) - count]
-
-def token_front(count=1):
-    return lex_token[lex_token.index(token) + count].replace(" ", "")
-
-def token_behind(count=1):
-    return lex_token[lex_token.index(token) - count].replace(" ", "")
-
-def str_token():
-    return_value = False
-    if token == '"' or token == "'": 
-        return True
-    else:
-        return False
-
-
-def switch(expression, casevar: dict, returning=False):
-    for k,v in cases.items():
-        if expression == k:
-            if returning == True:
-                return v
-            else:
-                v()
-
-
 result = []
-def newsplitter(arg):
-    token_splitters.append(arg)
 
 # Finishing up
 for line in line_list:
