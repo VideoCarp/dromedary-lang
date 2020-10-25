7dcd71d899a1780b944edbad336e01b514a1c82c
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-25 22:58:33 +1100

    Update error_handling.py

--- source/error_handling.py
+++ source/error_handling.py
@@ -1,7 +1,4 @@
 from functions import color_collection
 #print(f"{colors.color}Hi{colors.end}")
 
-def send(args,clr=colors.error):
-    print(f"{clr}{args}{colors.end}")
-
 #send("message", colors.color")
