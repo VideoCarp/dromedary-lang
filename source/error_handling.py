5eb15f9d1b65c77d9915eb6ec3145f634b1f7086
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-25 22:55:45 +1100

    Update error_handling.py

--- source/error_handling.py
+++ source/error_handling.py
@@ -1,15 +1,4 @@
-class colors:
-    header = '\033[95m'
-    blue = '\033[94m'
-    cyan = '\033[96m'
-    green = '\033[92m'
-    warning = '\033[93m'
-    error = '\033[91m'
-    end = '\033[0m'
-    bold = '\033[1m'
-    underlined = '\033[4m'
-    gray = '\u001b[30;1m'
-
+from functions import color_collection
 #print(f"{colors.color}Hi{colors.end}")
 
 def send(args,clr=colors.error):
