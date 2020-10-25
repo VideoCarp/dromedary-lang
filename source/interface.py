c0a4b8c686d0295fb6f48f849402679c89c9ce20
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-25 23:05:43 +1100

    Update interface.py

--- source/interface.py
+++ source/interface.py
@@ -1,28 +1,21 @@
-import lexfile as lexical
-class Colors:
-    header = '\033[95m'
-    blue = '\033[94m'
-    cyan = '\033[96m'
-    green = '\033[92m'
-    warning = '\033[93m'
-    error = '\033[91m'
-    end = '\033[0m'
-    bold = '\033[1m'
-    underlined = '\033[4m'
-    gray = '\u001b[30;1m'
-    default = ''
+import lexfile as lexer
+from functions import color_collection as colors
 
 
-def send(args,clr=Colors.default):
+def send(args,clr=colors.default):
     print(f"{clr}{args}{colors.end}")
 
 #send("message", Colors.color")
 
-send("Dromedary Language Compiler", Colors.bold)
+colors.send("Dromedary Language Compiler", colors.bold)
 
-print(f"Type a {Colors.warning}path{colors.end} to a script to compile it.",end='')
+print(f"Type a {colors.warning}{colors.bold}path{colors.end} to a script to compile it.
+      : ",end='')
 file = input()
 
 
+# send 'data' variable
 with open(file, "r") as f:
-    lexical.analyze(f.read())
+      colors.send("Sending data to lexical analyzer...", colors.green)
+      lexer.run(f.read())
+
