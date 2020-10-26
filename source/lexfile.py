7ef4010626c65683954cbeff16d10c06817f6640
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-26 19:50:06 +1100

    Hopefully fixed errors.

--- source/lexfile.py
+++ source/lexfile.py
@@ -9,78 +9,75 @@ def analyze(toparse=None):
     datawords = data.split(" ")
     lines = data.split("\n")
     class Lexicon:
-        SYMBOLS = {
-            '-': 'SUBTRACT',
-            '+': 'PLUS',
-            '*': 'MULTIPLY',
-            '%': 'MODULUS',
-            '/': 'DIVIDE',
-            '(': 'RPAREN',
-            ')': 'LPAREN',
-            '[': 'RBRACKET',
-            ']': 'LBRACKET',
-            '{': 'RBRACE',
-            '}': 'LBRACE',
-            '=': 'REASSIGN',
-            ':=': 'ASSIGN',
-            '==': 'EQUIVALENT',
-            '!=': 'UNEQUAL',
-            '>': 'GREATER',
-            '<': 'SMALLER',
-            '>=': 'GREATER_OR_EQUAL',
-            '<=': 'SMALLER_OR_EQUAL',
-            ',': 'COMMA'
+        SINGLECHAR = {
+            '-': 'ARITHMETIC',
+            '+': 'ARITHMETIC',
+            '*': 'ARITHMETIC',
+            '%': 'ARITHMETIC',
+            '/': 'ARITHMETIC',
+            '(': 'PAREN',
+            ')': 'PAREN',
+            '[': 'BRACKET',
+            ']': 'BRACKET',
+            '{': 'BRACE',
+            '}': 'BRACE',
+            '=': 'EQUAL',
+            '==': 'COMPARISON',
+            '!=': 'COMPARISON',
+            '>': 'COMPARISON',
+            '<': 'COMPARISON',
+            '>=': 'COMPARISON',
+            '<=': 'COMPARISON',
         }
-        KEYWORDS = {
-            'if': 'FLOW_IF',
-            'while': 'FLOW_WHILE',
-            'for': 'FLOW_FOR',
-            'function': 'ASSIGN_FUNCTION',
-            'in': 'IN_OPERATOR',
-            'true': 'BOOL_TRUE',
-            'false': 'BOOL_FALSE'
+        MULTICHAR = {
+            # multichar symbols
+            '==': 'COMPARISON',
+            '!=': 'COMPARISON',
+            '>=': 'COMPARISON',
+            '<=': 'COMPARISON'
+            # keywords
+            'if': 'CONTROL',
+            'while': 'CONTROL',
+            'for': 'CONTROL',
+            'true': 'BOOLEAN',
+            'false': 'BOOLEAN',
+            'int': 'STATIC',
+            'string': 'STATIC',
+            'char': 'STATIC',
+            'float': 'STATIC',
+            # functions
+            'length': 'BUILT_IN_FUNCTION',
+            'string': 'BUILT_IN_FUNCTION',
+            'integer': 'BUILT_IN_FUNCTION',
+            'print': 'BUILT_IN_FUNCTION',
+            'println': 'BUILT_IN_FUNCTION'
         }
-        FUNCTIONS = {
-            'length': 'LENGTH_FUNCTION',
-            'string': 'STRING_FUNCTION',
-            'integer': 'INTEGER_FUNCTION',
-            'print': 'PRINT_FUNCTION',
-            'sqrt': 'SQUARE_ROOT'
-        }
+       
+
     
     
     
     
     
     # FIX : doesn't work with more than one char.
-    for k,v in Lexicon.SYMBOLS.items():
+    for k,v in Lexicon.SINGLECHAR.items():
         for char_index in range(len(data) - 1):
             char = data[char_index]
-            if char == k:
+            nextchar = data[char_index + 1]
+            if char == k and nextchar != '=':
                for i in range(char_index + 1):
                    lexed.append('')
                lexed[char_index] = (char,v)
     
     
     
-    
-    #FIX : provides incorrect tag.
-    for k,v in Lexicon.KEYWORDS.items():
+    for k,v in Lexicon.MULTICHAR.items():
         for word_index in range(len(datawords) - 1):
             word = datawords[word_index]
-            if word == k:
-                for i in range(word_index):
+            if word == k and datawords[word_index + 1] != '(':
+                for i in range(word_index + 1):
                     lexed.append('')
-                lexed[word_index] = (word,v)
-    
-    #FIX : provides incorrect tag.
-    for k,v in Lexicon.FUNCTIONS.items():
-        for word_index in range(len(datawords) - 1):
-            word = datawords[word_index]
-            for i in range(word_index):
-                lexed.append('')
-            lexed[word_index] = (word,v)
-    
+                lexed[char_index] = (char,v)
     
     lexed = [value for value in lexed if value != '']
     # This just removes all empty tokens that were used to bypass index errors, or any useless tokens.
