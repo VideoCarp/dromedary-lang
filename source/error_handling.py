912e8be549ded0af82794036b9f0b3c5cec1fcf5
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-27 21:42:53 +1100

    Update error_handling.py

--- source/error_handling.py
+++ source/error_handling.py
@@ -1,4 +1,23 @@
 from functions import color_collection
+from functions import lexer_functions
+
 #print(f"{colors.color}Hi{colors.end}")
 
+def any_in(args):
+    for each in any_in:
+        if each in line:
+            return True
+
+typelist = ["int", "string", "char", "double", "float", "long"]
 #send("message", colors.color")
+
+identifiers = {}
+def handle(lexdata, linelist):
+    lexed = lexdata
+    lines = linelist
+    for token_index in range(len(lexed) - 1):
+        for line_index in range(len(lines - 1):
+            token = lexed[token_index]
+            line = lines[line_index]
+            print("Entering error handling...")
+            if any_in(typelist) and " = " in line:
