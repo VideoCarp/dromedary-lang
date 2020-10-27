60ef84197fb3d968f44e1c3aff8093aad5faaa54
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-27 20:54:05 +1100

    Update lexfile.py

--- source/lexfile.py
+++ source/lexfile.py
@@ -1,98 +1,37 @@
+import re
+import json
 def analyze(toparse=None):
-    if toparse is None or toparse == "":
+    if toparse is None or toparse.replace(" ", "") == "":
        raise Exception("Lexer did not get enough info.")
     print("Successfully entered lexical analyzer.")
-    data = toparse
+
+
     GSUB = {}
-    print("Going through substitues sheet...")
-    try:
-        with open("substitutes.json", "r") as f:
-            GSUB = json.loads(f.read())
-        for k,v in GSUB.items():
-            data = data.replace(k,v)
-        print("Went through substitues sheet.")
-    except:
-        print("Failed to use global substitues. May be empty..")
-    result = []
+    with open("config.json") as f:
+        GSUB = json.loads(f.read())
+
+    for k,v in GSUB.items():
+        toparse.replace(k,v)
+
+
+    data = toparse
     lexed = []
     lexdata = []
-    datawords = data.split(" ")
     lines = data.split("\n")
     class Lexicon:
-        SYMBOLS = {
-            '-': 'SUBTRACT',
-            '+': 'PLUS',
-            '*': 'MULTIPLY',
-            '%': 'MODULUS',
-            '/': 'DIVIDE',
-            '(': 'RPAREN',
-            ')': 'LPAREN',
-            '[': 'RBRACKET',
-            ']': 'LBRACKET',
-            '{': 'RBRACE',
-            '}': 'LBRACE',
-            '=': 'REASSIGN',
-            ':=': 'ASSIGN',
-            '==': 'EQUIVALENT',
-            '!=': 'UNEQUAL',
-            '>': 'GREATER',
-            '<': 'SMALLER',
-            '>=': 'GREATER_OR_EQUAL',
-            '<=': 'SMALLER_OR_EQUAL',
-            ',': 'COMMA'
-        }
-        KEYWORDS = {
-            'if': 'FLOW_IF',
-            'while': 'FLOW_WHILE',
-            'for': 'FLOW_FOR',
-            'function': 'ASSIGN_FUNCTION',
-            'in': 'IN_OPERATOR',
-            'true': 'BOOL_TRUE',
-            'false': 'BOOL_FALSE'
-        }
-        FUNCTIONS = {
-            'length': 'LENGTH_FUNCTION',
-            'string': 'STRING_FUNCTION',
-            'integer': 'INTEGER_FUNCTION',
-            'print': 'PRINT_FUNCTION',
-            'sqrt': 'SQUARE_ROOT'
-        }
+        ARITHMETICS = ["-", "+", "*", "%", "/"]
+        RIGHTLEFT = ["(", ")", "[", "]", "{", "}", r" "]
+        COMPARISONS = [">", "<"]
+
+    full = []
+    full = Lexicon.ARITHMETICS + Lexicon.RIGHTLEFT + Lexicon.COMPARISONS
+    ending = ""
+    for each in full:
+       nospace = "\\".join(full)
     
-    
-    
-    
-    
-    # FIX : doesn't work with more than one char.
-    for k,v in Lexicon.SYMBOLS.items():
-        for char_index in range(len(data) - 1):
-            char = data[char_index]
-            if char == k:
-               for i in range(char_index + 1):
-                   lexed.append('')
-               lexed[char_index] = (char,v)
-    
-    
-    
-    
-    #FIX : provides incorrect tag.
-    for k,v in Lexicon.KEYWORDS.items():
-        for word_index in range(len(datawords) - 1):
-            word = datawords[word_index]
-            if word == k:
-                for i in range(word_index):
-                    lexed.append('')
-                lexed[word_index] = (word,v)
-    
-    #FIX : provides incorrect tag.
-    for k,v in Lexicon.FUNCTIONS.items():
-        for word_index in range(len(datawords) - 1):
-            word = datawords[word_index]
-            for i in range(word_index):
-                lexed.append('')
-            lexed[word_index] = (word,v)
-    
-    
-    lexed = [value for value in lexed if value != '']
-    # This just removes all empty tokens that were used to bypass index errors, or any useless tokens.
+    lexed = re.split(fr"([{nospace}])", data)
+    for valueindex in range(len(lexed) - 1):
+        lexed[valueindex] = lexed[valueindex].replace("\n", "").replace("  ", "")
+    lexed = [value for value in lexed if value != '' and value != ' ']
     print(lexed)
     print("Exited lexical analyzer.")
