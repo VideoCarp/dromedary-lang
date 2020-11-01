82ba38dab87a2ebd84632213843e35fa0d99988b
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-11-01 18:02:40 +1100

    Update syntax_analyzer.py

--- source/syntax_analyzer.py
+++ source/syntax_analyzer.py
@@ -1,1 +1,5 @@
 def run(tokens: list, lines: list) -> str:
+    print("Entered syntax analyzer.")
+    for token_index in range(len(tokens) - 1):
+        for line_index in range(len(lines) - 1):
+            pass
