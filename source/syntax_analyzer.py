94407cd14d2688b60da2a216f087cbed398a309f
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-11-01 18:03:56 +1100

    Update syntax_analyzer.py

--- source/syntax_analyzer.py
+++ source/syntax_analyzer.py
@@ -2,6 +2,6 @@ def run(lexed: list, lines: list) -> str:
     print("Entered syntax analyzer.")
     for token_index in range(len(lexed) - 1):
         for line_index in range(len(lines) - 1):
-            token = lexed[token_index]
-            line = lines[line_index]
+            token: str = lexed[token_index]
+            line: str = lines[line_index]
             pass
