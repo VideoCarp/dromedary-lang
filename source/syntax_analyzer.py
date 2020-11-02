f2bbd9fd23b0d1dca5f5304c8233ba7e60fa751c
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-11-02 17:18:48 +1100

    Update syntax_analyzer.py

--- source/syntax_analyzer.py
+++ source/syntax_analyzer.py
@@ -1,13 +1,30 @@
 from functions import *
 from functions import varcollection as cat # category
+
 def run(lexed: list, lines: list) -> str:
     print("Entered syntax analyzer.")
+    resulting: list = []
     for token_index in range(len(lexed) - 1):
         for line_index in range(len(lines) - 1):
             token: str = lexed[token_index]
             line: str = lines[line_index]
                 if "#interop" not in line:
+                    resin: str = line # result_instance
                     if line.find("#") >= 0:
-                        line = line[:line.find("#")]
-                    if line.replace(" ", "").endswith("{") == False and line.replace(" ", "").endswith("}") == False:
-                        lines[line_index] = f"{line};"
+                        resin = resin[:resin.find("#")]
+
+                    if line.replace(" ", "").endswith("{") == False and line.replace(" ", "").endswith("}") == False \
+                    and line.replace(" ", "").endswith(";") == False:
+                        resin = f"{resin};"
+                        
+                    if token == "string":
+                        advance()
+                        # string hello = "Hello World!"
+                        # char hello[] = "Hello World!";
+                        resin = resin.r(resin, f"char {token}[] =")
+                        
+                        
+                 elif "#interop" in line:
+                        resulting.append(resin)
+
+                    
