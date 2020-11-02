a6a5f3b0f08902d933011ff4fddca58d4230da63
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-11-02 16:57:36 +1100

    Update lexer_functions.py

--- source/functions/lexer_functions.py
+++ source/functions/lexer_functions.py
@@ -4,22 +4,15 @@ def line_above(count=1):
 def line_below(count=1):
     return lines[line_index - count]
 
+def advance(count=1):
+    if token_index + count < len(lexed) and token_index + count>= 0:
+        token_index += count
+        token = lexed[token_index]
 
-def token_front(count=1):
-    return lexed[token_index + count]
-
-def token_behind(count=1):
-    return lexed[token_index - count]
-
-
-def switch(expression, casevar: dict, returning=False):
-    for k,v in cases.items():
-        if expression == k:
-            if returning == True:
-                return v
-            else:
-                v()
-
+def back(count=1):
+    if token_index - count < len(lexed) and token_index + count >= 0:
+        token_index -= count
+        token = lexed[token_index]
 
 def find_second(string, substring):
    return string.find(substring, string.find(substring) + 1)
